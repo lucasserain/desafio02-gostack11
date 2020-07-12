09ac0a714c05fe686e2dad63e90d3673c8285384
Lucas Serain <lucas.serain@outlook.com>
2020-07-12 13:37:22 -0300

    jojo

--- src/app.js
+++ src/app.js
@@ -1,33 +1,87 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id === id
+    );
+
+    if(findRepositoryIndex ===-1){
+      return response.status(400).json({error: 'Repositorio nao existente'});
+    }
+
+  const repository = {
+    title,
+    url,
+    techs,
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+  repositories[findRepositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs} = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id === id
+    );
+
+    if(findRepositoryIndex >= 0 ){
+      repositories.splice(findRepositoryIndex, 1);
+    }else{
+      return response.status(400).json({error: 'Repositorio nao existente'});
+    }
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repository = repositories.find(repository => repository.id === id)
+
+  if (!repository) {
+    return response.status(400).json({ error: 'Repository does not exists.' })
+  }
+
+  repository.likes = repository.likes + 1
+  return response.json(repository)
 });
 
 module.exports = app;
